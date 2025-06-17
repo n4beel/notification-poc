a69195a2181547b9b490a3cb6d9503ce89bc0c00
n4beel <nabeel3353045805@hotmail.com>
2025-06-17 21:16:30 +0500

    set up complete for trustamp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,115 +2,26 @@
 
 import Image from "next/image";
 import styles from "./page.module.css";
+import * as PusherPushNotifications from "@pusher/push-notifications-web";
 import { useEffect } from "react";
-import OneSignal from "react-onesignal";
 
 export default function Home() {
 
   useEffect(() => {
-    // Ensure this code runs only on the client side
-    if (typeof window !== 'undefined') {
-      OneSignal.init({
-        appId: process.env.NEXT_ONESIGNAL_APP_ID || "47d72783-3c4c-4a2e-b627-36600985e646",
-        safari_web_id: process.env.NEXT_ONESIGNAL_SAFARI_WEB_ID || "web.onesignal.auto.2ce6e088-bd06-4a6b-8c58-d83c247eb259",
-        // You can add other initialization options here
-        notifyButton: {
-          enable: true,
-        },
-        // Uncomment the below line to run on localhost. See: https://documentation.onesignal.com/docs/local-testing
-        // allowLocalhostAsSecureOrigin: true
-      });
-    }
-  }, []);
+    const beamsClient = new PusherPushNotifications.Client({
+      instanceId: '',
+    });
+
+    beamsClient.start()
+      .then(() => beamsClient.addDeviceInterest('684ab37f156a5b55e993e9b4')) // use user's id here
+      .then(() => console.log('Successfully registered and subscribed!'))
+      .catch(console.error);
+  }, [])
+
 
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      Notification PoC
     </div>
   );
 }
